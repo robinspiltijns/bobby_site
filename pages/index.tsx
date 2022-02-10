8da350d3d6996ba73df24a7709d81b9a1ab5e92b
robinspitlijns <spiltijnsrobin@gmail.com>
2022-02-10 15:26:13 +0100

    Add cover art

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
 import logo from '../public/logo.svg'
+import art from '../public/art.svg'
 
 const Home: NextPage = () => {
   return (
     <div className='m-3'>
       <Image src={logo} alt={'Bobby logo'}/>
       <div className='flex flex-row'>
-        <div className='flex flex-col space-y-5 w-1/3 px-11'>
-          <h1 className='text-4xl font-semibold text-white'>
-            Freelance <br/>
-            Product   <br/>
-            Engineer.  <br/>
-          </h1>
+        <div className='flex flex-col space-y-8 w-1/3 px-11'>
+          <div className='flex flex-col space-y-3'>
+            <div className='text-6xl font-semibold text-white'>Freelance</div>
+            <div className='text-6xl font-semibold text-purple'>Product</div>
+            <div className='text-6xl font-semibold text-white'>Engineer.</div>
+          </div>
           <p className='font-light'>
             Improving small businesses&apos;s    <br/>
             results through great software. <br/>
@@ -20,10 +21,14 @@ const Home: NextPage = () => {
             idea into a ready-to-go design, <br/>
             for free!
           </p>
-          <button className='bg-purple border-purple border-4  p-2 rounded-xl font-semibold'>Reach out</button>
-          <button className='border-purple p-2 border-4 rounded-xl text-purple font-semibold'>View services</button>
+          <div className='flex flex-col space-y-5 w-fit'>
+            <button className='bg-purple border-purple border-4 px-6 py-2 rounded-xl font-semibold'>Reach out</button>
+            <button className='border-purple px-6 py-2 border-4 rounded-xl text-purple font-semibold'>View services</button>
+          </div>
         </div> 
-        <div className='w-2/3'></div>
+        <div className='w-2/3'>
+          <Image src={art} alt='cover art'/>
+        </div>
       </div>
     </div>
   )
