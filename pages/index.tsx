1c82e0223194b579c799de3df5c76820a2808600
robinspitlijns <spiltijnsrobin@gmail.com>
2022-02-10 14:56:11 +0100

    add initial html and styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
+import logo from '../public/logo.svg'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className='underline italic text-9xl text-white'>Bobby test branch.</h1>
-      <button>Submit</button>
+    <div className='m-3'>
+      <Image src={logo} alt={'Bobby logo'}/>
+      <div className='flex flex-row'>
+        <div className='flex flex-col space-y-5 w-1/3 px-11'>
+          <h1 className='text-4xl font-semibold text-white'>
+            Freelance <br/>
+            Product   <br/>
+            Engineer.  <br/>
+          </h1>
+          <p className='font-light'>
+            Improving small businesses's    <br/>
+            results through great software. <br/>
+            Reach out and I will turn your  <br/>
+            idea into a ready-to-go design, <br/>
+            for free!
+          </p>
+          <button className='bg-purple border-purple border-4  p-2 rounded-xl font-semibold'>Reach out</button>
+          <button className='border-purple p-2 border-4 rounded-xl text-purple font-semibold'>View services</button>
+        </div> 
+        <div className='w-2/3'></div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      purple: '#823DC8'
+    },
     extend: {},
   },
   plugins: [],
