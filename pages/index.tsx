c6595c9184dcf047c0178f1188ec8018140df783
robinspitlijns <spiltijnsrobin@gmail.com>
2022-02-12 20:11:39 +0100

    smaller button

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
     <div>
       <div className='fixed top-0 z-10 px-7 w-full flex flex-row justify-between items-center bg-dark border-b-2 border-gray'>
         <Image width='90' src={logo} alt={'Bobby logo'}/>
-        <button className='bg-purple border-purple border-4 px-2 py-1 rounded-xl font-semibold lg:hidden'>Reach out</button>
+        <button className='bg-purple border-purple border-4 px-2 py-1 rounded-md font-semibold lg:hidden text-sm'>Reach out</button>
         <div className='flex flex-row space-x-4 hidden lg:block'>
           <button className='font-semibold hover:underline'>Home</button>
           <button className='font-semibold hover:underline'>Services</button>
