2692fb6fe92b5d076d6ed11456202c032e01a00e
robinspitlijns <spiltijnsrobin@gmail.com>
2022-02-12 12:37:51 +0100

    hide hero on small screens and add side-margins

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
       <div className='mt-3 mx-7 absolute'>
         <Image width='100' src={logo} alt={'Bobby logo'}/>
       </div>
-      <div className='flex flex-col justify-center h-screen'>
+      <div className='flex flex-col justify-center h-screen mx-12'>
         <div className='flex flex-row justify-center space-x-20'>
           <div className='flex flex-col space-y-9'>
               <div className='flex flex-col space-y-3'>
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
                 <button className='border-purple px-6 py-2 border-4 rounded-xl text-purple font-semibold'>View services</button>
               </div>
             </div> 
-            <div className='imageContainer'>
+            <div className='imageContainer hidden lg:block'>
               <Image height='100%' src={art} alt="Bobby cover art"/>
             </div>
         </div>
