cc78658296d53b70c0e61e48149ad090c3e71500
robinspitlijns <spiltijnsrobin@gmail.com>
2022-02-13 14:06:12 +0100

    hamburger

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,11 @@ const NavBar = () => {
     return (
         <div className='fixed top-0 z-10 w-full flex flex-row justify-between items-center bg-dark border-b-2 border-gray px-3 lg:px-7 '>
         <Image width='90' src={logo} alt={'Bobby logo'}/>
-        <button className='bg-purple border-purple border-4 px-2 py-1 rounded-md font-semibold lg:hidden text-sm'>Reach out</button>
+        <div className="space-y-2 lg:hidden">
+          <div className="w-8 h-1 bg-white rounded"></div>
+          <div className="w-8 h-1 bg-white rounded"></div>
+          <div className="w-8 h-1 bg-white rounded"></div>
+        </div>        
         <div className='flex flex-row space-x-4 hidden lg:block'>
           <button className='font-semibold hover:underline'>Home</button>
           <button className='font-semibold hover:underline'>Services</button>
