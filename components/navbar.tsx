95b62bb97ed6b656f788aff16526f7a1610da164
robinspitlijns <spiltijnsrobin@gmail.com>
2022-02-13 21:01:26 +0100

    add hamburger dropdown

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,26 +1,36 @@
 import Image from "next/image"
+import { useState } from "react"
 import logo from '../public/logo.svg'
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const onMenuClick = () => {setMenuOpen(!menuOpen)}
   return (
     <div className="fixed top-0 z-10 w-full">
       <div className='w-full flex flex-row justify-between items-center bg-dark border-b-2 border-gray px-3 lg:px-7 '>
         <Image width='90' src={logo} alt={'Bobby logo'} />
-        <div className="space-y-2 lg:hidden">
+        <div onClick={onMenuClick} className="space-y-2 lg:hidden">
           <div className="w-8 h-1 bg-white rounded"></div>
           <div className="w-8 h-1 bg-white rounded"></div>
           <div className="w-8 h-1 bg-white rounded"></div>
         </div>
         <div className='flex flex-row space-x-4 hidden lg:block'>
           <button className='font-semibold hover:underline'>Home</button>
-          <button className='font-semibold hover:underline'>Services</button>
-          <button className='font-semibold hover:underline'>Portfolio</button>
           <button className='font-semibold hover:underline'>Contact</button>
         </div>
       </div>
-      <div className="flex flex-row w-full justify-center">
-
-      </div>
+      {menuOpen ?
+        <div className="flex flex-col w-full items-center z-10 bg-dark lg:hidden">
+          <div className="w-full py-5 flex flex-row justify-center border-b-2 border-gray">
+            <button className='font-semibold hover:underline'>Home</button>
+          </div>
+          <div className="w-full py-5 flex flex-row justify-center border-b-2 border-gray">
+            <button className='font-semibold hover:underline'>Contact</button>
+          </div>
+        </div>
+        :
+        null
+      }
     </div>
 
   )
