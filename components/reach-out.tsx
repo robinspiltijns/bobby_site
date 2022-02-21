fa24c104c11e1b10194270d659e9868ede621606
robinspitlijns <spiltijnsrobin@gmail.com>
2022-02-21 18:23:50 +0100

    WIP (sendgrid gives uncaught error)

--- components/reach-out.tsx
+++ components/reach-out.tsx
@@ -1,15 +1,16 @@
+import sgMail, { MailDataRequired } from '@sendgrid/mail'
+
 const ReachOut = () => {
     const onClick = async () => {
         const res = await fetch('/api/hello', {
             method: 'POST',
             body: JSON.stringify({
-                email: 'test@test.test',
-                message: 'This is a test message'
+                email: 'spiltijnsrobin@gmail.com',
+                message: 'This is a test message sent from the frontend.'
             })
         })
         console.log(res);
     }
-
     return (
         <div onClick={onClick}>
             reach out
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,22 +1,43 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import sgMail, { MailDataRequired } from '@sendgrid/mail'
 
 interface RequestBody {
-  emailAdress: string,
-  message: string
+  email: string,
+  message: string,
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+function isRequestBody(obj: any): obj is RequestBody {
+  if (
+    typeof(obj.email) === 'string' && 
+    typeof(obj.message) === 'string'
+    ) return true
+  return false
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<string>) {
   if (req.method !== 'POST') {
     res.status(501).json("NOT IMPLEMENTED")
   }
-  const requestBody: RequestBody = JSON.parse(req.body)
-  console.log(requestBody);
-  console.log(process.env.KEY);
-  res.status(200).json("OK")
 
+  if (process.env.KEY === undefined) 
+    throw new Error("The sendgrid API key environment variable is not present.")
+
+  const requestBody: RequestBody = JSON.parse(req.body)
+  if (isRequestBody(requestBody)) {   
+    sgMail.setApiKey(process.env.KEY)
+    const msg: MailDataRequired = {
+      to: requestBody.email,
+      from: 'test@bobby.com',
+      subject: 'Sendgrid test email',
+      text: requestBody.message
+    }
+    try {
+      sgMail.send(msg) 
+      res.status(200).json("OK")
+    } catch (error) {
+      res.status(500).json("Something went wrong while sending the email.")
+    }
+  } else {
+    res.status(400).json("Request body not properly formatted")
+  }
 }
